207af36b2b4f19402304bc62b2258a5eea52e67e
ABCD <rachelwin6@gmail.com>
2025-06-14 17:37:26 +0900

    git commit push 테스트

---
+++ git-test.py
@@ -0,0 +1,13 @@
+"""
+0. init - 로컬 git 저장소 초기화
+
+1. git add - 파일 추가
+
+2. git commit -m "Inital Commit" : 변경사항 로그 저장
+
+(push 이전)
+3. git remote add 브랜치명 url: 원격저장소 등록
+
+4. git push : 원격저장소 업로드 또는 업데이트
+
+"""
